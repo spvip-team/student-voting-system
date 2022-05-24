45455f44220f966108b8a9d5888b0ade3e9816a9
Nobantu Matyobeni <nobantu.matyobeni@sp-vip.co.za>
2022-05-24 13:18:31 +0200

    initial commit

---
+++ src/main/java/za/co/spvip/student/voting/system/StudentVotingSystemApplication.java
@@ -0,0 +1,13 @@
+package za.co.spvip.student.voting.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentVotingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentVotingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/spvip/student/voting/system/StudentVotingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.spvip.student.voting.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentVotingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
